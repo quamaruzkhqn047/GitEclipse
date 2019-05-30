e86bf0ac994c369bba719da0cee986729d691212
quamaruzkhqn047 <quamaruzkhan047@gmail.com>
2019-05-30 16:44:07 +0530

    hii

---
+++ DemoGit/src/Employee.java
@@ -0,0 +1,25 @@
+
+public class Employee {
+	private int empid;
+	private String ename;
+	private double esal;
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public double getEsal() {
+		return esal;
+	}
+	public void setEsal(double esal) {
+		this.esal = esal;
+	}
+
+}
