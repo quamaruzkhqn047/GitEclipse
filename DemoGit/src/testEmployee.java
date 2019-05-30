9701dad37a33b593a8e325658a2e29e0b86467a8
quamaruzkhqn047 <quamaruzkhan047@gmail.com>
2019-05-30 16:52:32 +0530

    hiiiii

---
+++ DemoGit/src/testEmployee.java
@@ -0,0 +1,12 @@
+
+public class testEmployee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee e1=new Employee();
+		e1.setEname("khan");
+		e1.setEmpid(101);
+		e1.setEsal(1010101);
+	}
+
+}
